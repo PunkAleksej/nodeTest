ce83eb3a2b025813a66924546fc065bca3566cc9
Alex Dudnikov <alekdudnikov@mail.ru>
2022-06-23 18:05:55 +0300

    postman test

--- index.js
+++ index.js
@@ -2,40 +2,64 @@ import express from "express"
 import path from "path"
 import { registration, loginCheck } from "./middlewares.js"
 
-const PORT = 3000;
+const PORT = 4000;
 
 const app = express()
 
 const __dirname = path.resolve()
 
 app.use(express.static(__dirname + '/static'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-app.get('/', function(request, response) {
-    response.sendFile(path.resolve(__dirname, "static", "first.html"))
+app.get('/', function (request, response) {
+    // response.sendFile(path.resolve(__dirname, "static", "first.html"))
+    response.sendFile(`${__dirname}/static/first.html`)
 })
 
-
-
-
-
-const urlencodedParser = express.urlencoded({extended: false});
-app.post("/", urlencodedParser, function (request, response) {
-    if(!request.body) return response.sendStatus(400);
-    loginCheck(request.body)
-    //console.log(request.body);
-    //response.send(`${request.body.userName} - ${request.body.userAge}`);
-});
-   
-app.post("/registration.html", urlencodedParser, function (request, response) {
-    if(!request.body) return response.sendStatus(400);
+app.post("/", function (request, response) {
+    if (!request.body) return response.sendStatus(400);
     loginCheck(request.body)
 
 });
 
+app.post("/registration", function (request, response) {
+    try {
+
+        const status = registration(request.body)
+        response.json({
+            status,
+        })
+    } catch (error) {
+        console.error(error)
+        res.sendStatus(500);
+    }
+});
+
+app.post('/test/:id', (req, res) => {
+    try {
+        const { email } = req.body;
+        const { page } = req.query;
+        const { id } = req.params;
+        console.log('email:', email);
+        console.log('page:', page);
+        console.log('id:', id);
+
+        res.json({
+            email,
+            page,
+            id,
+        })
+    } catch (err) {
+        console.error(err);
+        res.sendStatus(500);
+    }
+});
+
+//npm run serve
 // app.get('/', function(req, res) {
 //     res.sendFile(path.resolve(__dirname, "static", "first.html"))
 // })
-
 
 
 app.listen(PORT, () => {
--- middlewares.js
+++ middlewares.js
@@ -1,22 +1,76 @@
-import colors from "colors"
-export function requestTime(req, res, next) {
-  req.requestTime = Date.now()
-
-
-  next()
-}
-
-export function logger(req, res, next) {
-  const loginButton = document.querySelector(".loginButton");
-  loginButton.addEventListener("click", function (event) {
-    loginCheck()
-  })
-  next()
-}
-
 export function registration(user) {
+  //console.log(user)
+  const { regName, regSurname, regMail, 
+  regPassword, regRepPassword } = user;
+
+ 
+
+  //============Name=================
+
+  if ( 4 > regName.length && regName.length < 20 ) {
+    return('The length of the name should be from 4 to 20 characters')
+  }
+
+  if (/[^a-zA-Z]/.test(regName)) {
+    return('The name should consist only of letters')
+  }
+
+  //============Surname==============
+
+  if ( 4 > regSurname.length && regSurname.length < 20 ) {
+    return('The length of the surname should be from 4 to 20 characters')
+  }
+
+  if (/[^a-zA-Z]/.test(regSurname)) {
+    return('The surname should consist only of letters')
+  }
+  //============Password==============
+
+
+  if (regPassword !== regRepPassword) {
+    return('Passwords dont match')
+
+  }
+
+
+  if (8 > regPassword.length && regPassword.length < 20) {
+    return('The length of the password should be from 4 to 20 characters')
+  }
+
+  if (!checkPassword(regPassword)) {
+    return('Wrong password')
+  }
   
+  function checkPassword(str)
+  {
+    var re = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])\w{6,}$/;
+    return re.test(str);
+  }
+
+
+  //============Mail==================
+  function validateEmail(email) {
+      var re = /\S+@\S+\.\S+/;
+      return re.test(email);
+  }
+
+  if (!validateEmail(regMail)) {
+    return('Wrong email')
+  }
+
+  return ('Registered')
+
 }
+
+// {
+//   regName: 'Alex',
+//   regSurname: 'Dudnikov',
+//   regMail: 'alekdudnikov@mail.ru',
+//   regPassword: '89185209977',
+//   regRepPassword: '89185209977'
+// }
+
+
 
 
 export function loginCheck (user) {
